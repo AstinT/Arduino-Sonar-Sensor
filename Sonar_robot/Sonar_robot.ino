b49c273b7426bdf025b534418191bb21fcefa6d0
Astin Tiddy <Astin8@hotmail.co.nz>
2015-11-19 22:01:40 +1300

    Worked on code

--- Sonar_robot/Sonar_robot.ino
+++ Sonar_robot/Sonar_robot.ino
@@ -2,44 +2,88 @@
 #include <RedBotSoftwareSerial.h>
 
 RedBotMotors motors;
+RedBotSensor leftSensor = RedBotSensor(A1);
+RedBotSensor centerSensor = RedBotSensor(A4);
+RedBotSensor rightSensor = RedBotSensor(A5);
 
+//Sensors
 int sensorOneTrig = 9;
 int sensorTwoTrig = 11;
 int sensorOneEcho = 3;
 int sensorTwoEcho = 10;
 
+//Sonar dist
 float sensorOneDist;
 float sensorTwoDist;
 
+//Line sensor
+int valLeft;
+int valCenter;
+int valRight;  
+
+//States
+boolean followWall;
+boolean findLine;
+
 void setup ()
 {
-  Serial.begin (9600);
   pinMode(sensorOneTrig, OUTPUT);
   pinMode(sensorTwoTrig, OUTPUT);
   pinMode(sensorOneEcho, INPUT);
   pinMode(sensorTwoEcho, INPUT);
+  
+  followWall = true;
 }
 
 void loop ()
 {
-  sensorOneDist = readSonar(sensorOneTrig, sensorOneEcho);
-  sensorTwoDist = readSonar(sensorTwoTrig, sensorTwoEcho);
-  
-  delay(100);
-  
-  if (sensorTwoDist > 10)
+  if (followWall == true)
   {
-    driveCloser(100, 100);
+    sensorOneDist = readSonar(sensorOneTrig, sensorOneEcho);
+    sensorTwoDist = readSonar(sensorTwoTrig, sensorTwoEcho);    
+   
+    if (sensorOneDist < 25)
+    {     
+      sensorOneDist = readSonar(sensorOneTrig, sensorOneEcho);
+      if (sensorOneDist < 10)
+      {
+        followWall = false;
+        findLine = true;
+      }
+      else
+      {
+        driveStraight(100, 100);
+      }
+    }
+    else if (sensorTwoDist > 15)
+    {
+      driveCloser(100, 100);
+    }
+    else if (sensorTwoDist < 10)
+    {
+      driveAway(100, 100);
+    }    
+    else
+    {
+      driveStraight(100, 100);
+    }
+    delay(50);
   }
-  else if (sensorTwoDist < 5)
+  else if (findLine == true)
   {
-    driveAway(100, 100);
-  }
-  else
-  {
-    driveStraight(100, 100);
+    pivotRight90();
+    while((valLeft < 800) || (valCenter < 800) || (valRight < 800))
+    {
+      driveStraight(100, 50);
+      valLeft = leftSensor.read();
+      valCenter = centerSensor.read();
+      valRight = rightSensor.read();
+    }
+    
+    findLine = false;
   }
 }
+
 
 // Drive straight function.
 // Takes two parameters power and time.
@@ -54,17 +98,26 @@ void driveStraight(int power, int time)
 void driveCloser(int power, int time)
 {
   motors.rightDrive(power);
-  motors.leftDrive(power*0.90);
+  motors.leftDrive((power*0.72) + 20);
   delay(time);
-  motors.brake();  
+  motors.brake();
 }
 
 void driveAway(int power, int time)
 {
-  motors.rightDrive(power + 40);
+  motors.rightDrive(power + 20);
   motors.leftDrive(power*0.72);
   delay(time);
   motors.brake();
+}
+
+void pivotRight90()
+{
+  motors.leftDrive(150);
+  motors.rightDrive((-150)*0.63);
+  delay(280);
+  motors.brake();
+  delay(20);
 }
 
 float readSonar (int trigPin, int echoPin)
